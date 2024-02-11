d4f35d231845bc1a6f8d221ab35a0f13392da74a
deathstroke-Assassin <moh2004amed@gmail.com>
2024-02-11 14:22:04 -0800

    gh

--- models/base_model.py
+++ models/base_model.py
@@ -15,28 +15,28 @@ class BaseModel:
 
         """Initializes instance attributes"""
 
-        if kwargs is not None and kwargs =! {}:
+        if kwargs is not None and kwargs is not {}:
             for key in kwargs:
                 if key == 'created_at':
                     self.__dict__["created_at"] = datetime.strptime(
                             kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key = ["updated_at"]:
+                elif key == ["updated_at"]:
                     self.__dict__["updated_at"] = datetime.strptime(
                             kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
-            self.updated_at = datetime.now
-            self.created_at = datetime.now
+            self.updated_at = datetime.now()
+            self.created_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
 
         """string"""
 
-        return "[{}] ({}) {}".format(
-                type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+                            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updating updated_at"""
